488f3fa9b6139465fd483d9bbb7869240c0bfa28
Jani Mikkonen <jani.mikkonen@whitesheep.fi>
2015-03-25 17:30:22 +0200

    Add assets

---
+++ src/TimePickerAsset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace janisto\timepicker;
+
+class TimePickerAsset extends \yii\web\AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@bower/jqueryui-timepicker-addon';
+
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\jui\JuiAsset'
+    ];
+
+    /**
+     * @var string jQuery Timepicker language
+     */
+    public $language;
+
+    /**
+     * @inheritdoc
+     */
+    public function registerAssetFiles($view)
+    {
+        $this->css[] = 'jquery-ui-timepicker-addon' . (YII_DEBUG ? '' : '.min') . '.css';
+        $this->js[] = 'jquery-ui-timepicker-addon' . (YII_DEBUG ? '' : '.min') . '.js';
+
+        if ($this->language !== null) {
+            $this->js[] = 'i18n/jquery-ui-timepicker-' . $this->language . '.js';
+        }
+
+        parent::registerAssetFiles($view);
+    }
+}
